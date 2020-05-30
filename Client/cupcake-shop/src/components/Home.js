154a8912eab7cf426c1227899692c9209a851648
Lawrence Obote <dev.obote@gmail.com>
2020-05-29 23:02:50 -0400

    added random cupcake img as background

--- Client/cupcake-shop/src/components/Home.js
+++ Client/cupcake-shop/src/components/Home.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
 import NavigationBar from './NavigationBar'
+import axios from 'axios';
 
 export default class Home extends Component {
+    state = {
+        backgroundImage: ''
+    }
+    componentDidMount(){
+         const call = async () =>{
+            return axios.get(`https://api.unsplash.com/photos/random?&query=cupcake&client_id=4_-191W_jUgFQg_ujNLY9H3gpr7zGA4yY8ffZXQoJO0`)
+            .then(response => {
+                const img = response.data.urls.regular;
+                console.log(response.data.urls.regular);
+                this.setState({
+                    backgroundImage: img
+                })
+                return response.data.urls.regular;
+
+            })
+        }
+        call();
+        
+    }
     render() {
+        console.log(this.call);
         return (
             <div>
                 <NavigationBar/>
+                <div class="parallax" style={{backgroundImage: `url(${this.state.backgroundImage})` }}>Hello</div>
                 
             </div>
         )
