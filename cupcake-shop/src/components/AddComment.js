41abf8f6b07629feecf6e98f3bab858969897c83
Lawrence Obote <dev.obote@gmail.com>
2020-06-02 11:14:32 -0400

    added addcomment section back

--- cupcake-shop/src/components/AddComment.js
+++ cupcake-shop/src/components/AddComment.js
@@ -8,11 +8,7 @@ export default function AddComment() {
     let [comments, setComments] = React.useState([])
     const [newComment, setNewComment] = React.useState()
 
-    // React.useEffect(() => {
-    //     const getData = async () {
-    //          response = axios.get('http://localhost:8090/cupcakeShop/v1/comments')
-    //     }
-    // })
+    
     const getData = async () => {
         setComments = axios.get(`/cupcakeShop/v1/comments`);
         
@@ -42,10 +38,10 @@ export default function AddComment() {
     } )
     return (
         <div>
-            {/* <textarea id="new-comment-box" value={newComment}
+            <textarea id="new-comment-box" value={newComment}
                     onChange={e => setNewComment(e.target.value)}
                     />
-            <button className="button text-white" id="create-button" onClick={onCreate}>Create</button> */}
+            <button className="button text-white" id="create-button" onClick={onCreate}>Create</button>
             {comments.map(comment =>(
                 <li id="list-comment" key={comment.id}>
                     <Comment comment={comment}></Comment>
--- cupcake-shop/src/components/Cupcakes.js
+++ cupcake-shop/src/components/Cupcakes.js
@@ -12,7 +12,6 @@ class Cupcakes extends Component {
     constructor(props) {
         super(props);
         this.state = {cupcakes: [], isLoading: true};
-        // this.remove = this.remove.bind(this);
         this.getCupcakes = this.getCupcakes.bind(this);
     }
 
@@ -41,41 +40,15 @@ class Cupcakes extends Component {
             <NavigationBar/>
             <Row>
             {this.state.cupcakes.map(cupcake => 
-            <Col md="4">
-            <Item name={cupcake.name} price={`$` +`${cupcake.price}`} image={cupcake.imageURL}/>
-            </Col>   
+                <Col md="4">
+                <Item name={cupcake.name} price={`$` +`${cupcake.price}`} image={cupcake.imageURL}/>
+                </Col>   
             )}
-        </Row>
-    {/* // return (
-    //     <div>
-    //         <NavigationBar></NavigationBar>
-    //         <Row className="side-margin">
-    //             <Col md="4">
-    //                 <Item name={`${this.state.data[0]}`} price={"$10"} image={"https://images.unsplash.com/photo-1567022432449-5cdf87e209a4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"}></Item>
-    //             </Col>
-    //             <Col md="4">
-    //             <Item name={"Chocolate"} price={"$3"} image={"https://images.unsplash.com/photo-1583625494781-8f80e36bd727?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"}></Item>
-    //             </Col>
-    //             <Col md="4">
-    //             <Item name={"Red-Velvet"} price={"$3"} image={"https://images.unsplash.com/photo-1582241785274-2009ebb9b440?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"}></Item>
-    //             </Col>
-    //         </Row>
-    //         <Row className="side-margin">
-    //             <Col md="4">
-    //                 <Item name={"Strawberry"} price={"$3"} image={"https://images.unsplash.com/photo-1555684655-49a1c6b94dd1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"}></Item>
-    //             </Col>
-    //             <Col md="4">
-    //             <Item name={"Banana"} price={"$3"} image={"https://images.unsplash.com/photo-1576618148400-f54bed99fcfd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"}></Item>
-    //             </Col>
-    //             <Col md="4">
-    //             <Item name={"Carrot"} price={"$3"} image={"https://images.unsplash.com/photo-1487124504955-e42a39e11aaf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1012&q=80"}></Item>
-    //             </Col>
-    //         </Row>
-    //     </div>
-    // ) */}
+            </Row>
+    
 
-    <AddComment></AddComment>
-    </div>
+            <AddComment></AddComment>
+        </div>
         ) 
 }
 }
